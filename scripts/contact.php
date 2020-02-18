6c109773a2b13db9abd1064de3566aa16983b07c
siherrmann <Simon.He@gmx.de>
2020-02-18 16:45:46 +0100

    smaller comments

--- scripts/contact.php
+++ scripts/contact.php
@@ -6,8 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $antwortDaten = json_decode($antwortJSON);
 
   if (!$antwortDaten->success) {
-    # ReCaptcha error, set a 403 (forbidden) response code.
-    http_response_code(403);
+    http_response_code(403); #403=okay
     echo "There was a problem with your submission, please try again.";
     exit;
   } else {
@@ -19,13 +18,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail_to = "contact@erikbent.de";
 
     $success = mail($mail_to, "$subject - Nachricht von $name", $message, "From: $email");
-    if ($success) {
-        # Set a 200 (okay) response code.
-        http_response_code(200);
+    if ($success) {.
+        http_response_code(200); #200=okay
         echo "Thank you for contacting us, $name. We will try to reply within 24 hours.";
     } else {
-        # Set a 500 (internal server error) response code.
-        http_response_code(500);
+        http_response_code(500); #500=okay
         echo "We are sorry but the email did not go through.";
     }
 
@@ -39,18 +36,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $success = mail($email, $subjectResponse, $messageResponse, implode("\r\n", $headerResponse));
     if ($success) {
-        # Set a 200 (okay) response code.
-        http_response_code(200);
+        http_response_code(200); #200=okay
         echo "Thank You! Your message has been sent.";
     } else {
-        # Set a 500 (internal server error) response code.
-        http_response_code(500);
+        http_response_code(500); #500=okay
         echo "Oops! Something went wrong, we couldn't send your message.";
     }
   }
 } else {
-  # No POST error, set a 403 (forbidden) response code.
-  http_response_code(403);
+  http_response_code(403); #403=okay
   echo "There was a problem with your submission, please try again.";
 }
 ?>
