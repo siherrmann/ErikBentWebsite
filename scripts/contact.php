baee480d3c901b654c037db02e2a1bceb70fab74
siherrmann <Simon.He@gmx.de>
2020-02-20 16:11:15 +0100

    check for valid email and message length

--- scripts/contact.php
+++ scripts/contact.php
@@ -15,6 +15,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
+    if (strpos($email, '@') == false) {
+      http_response_code(403); #403=forbidden
+      echo "You have to put in a valid eamil address.";
+      exit;
+    }
+    if (strlen($message)<10) {
+      http_response_code(403); #403=forbidden
+      echo "Do you really want to send me such a short message?";
+      exit;
+    }
+
     $mail_to = "contact@erikbent.de";
 
     $success = mail($mail_to, "$subject - Nachricht von $name", $message, "From: $email");
@@ -27,8 +38,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
-    $subjectResponse = 'Ihre Nachricht an Erik Bent';
-    $messageResponse = 'Vielen Dank für Ihre Nachricht, wir werden uns schnellstmöglich bei Ihnen melden.';
+    $subjectResponse = 'Your message to Erik Bent';
+    $messageResponse = 'Thank you for your message, we will get back to you as soon as possible.';
     // für HTML-E-Mails muss der 'Content-type'-Header gesetzt werden
     $headerResponse[] = 'MIME-Version: 1.0';
     $headerResponse[] = 'Content-type: text/plain; charset=utf-8';
